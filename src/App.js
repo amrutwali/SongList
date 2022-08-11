0c35ae0b00d3386e945bd7c1fd36818c927aa130
waliamar <amrutwali@gmail.com>
2022-08-11 16:18:20 +0530

    Added router
    Added router page to app using react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import Home from "./Components/Home";
+// Router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Components
+import AddArtist from "./Components/AddArtist";
+import AllSongs from "./Components/AllSongs";
+import AllArtists from "./Components/AllArtists";
 
-function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
-}
+// Components
+import Home from "./Components/Home";
+import NavbarComponent from "./Components/Navbar/NavbarComponent";
+import NotFound from "./Components/NotFound";
+import SongDetails from "./Components/SongDetails";
+
+const App = () => (
+  <Router>
+    <NavbarComponent />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/songs/:songID" element={<SongDetails />} />
+      <Route path="/songs" element={<AllSongs />} />
+      <Route path="/artists" element={<AllArtists />} />
+      <Route path="/newartist" element={<AddArtist />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
